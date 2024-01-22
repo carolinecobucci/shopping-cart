ca123dfc2ac05c17f4422447eda984c465b91a51
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2024-01-22 13:02:57 -0300

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import { IoCartOutline } from "react-icons/io5";
+import Catalog from "./components/Catalog";
+import Cart from "./components/Cart";
+import ThankYouPage from "./components/ThankYouPage";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <nav className="flex justify-between my-3 px-48">
+        <Link to="/" className="font-semibold">
+          Catálogo
+        </Link>
+        <Link to="/cart">
+          <IoCartOutline size={25} />
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Catalog />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/thank-you" element={<ThankYouPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
